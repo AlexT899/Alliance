3e16629399a9520abc4aad86eb10d39bbe35031c
AlexT899 <lexa500@yahoo.ie>
2024-11-13 19:25:35 +0000

    +

--- handler.php
+++ handler.php
@@ -1,5 +1,11 @@
 <?php 
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
+
+$token = "8014620879:AAGDkn5AyhqN3IdVHzhhhoMtgFqaRUuc04w";
+$chat_id = "-4575687290";
+
+fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$user_name}");
+
 echo "Привет, " . $user_name . "<br>";
-echo "Ваш Телефон: <b> " . $user_phone . "</b>" ;
+echo "Ваш Телефон: <b> " . $user_phone . "</b>";
