029f52dad3c8d79c54e204c45ece59af08b97ad7
AlexT899 <lexa500@yahoo.ie>
2024-11-13 21:42:26 +0000

    telegram

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="bg-grey section-cta">
       <img src="img/cta-bg.png" alt="cta" class="cta-image">
       <div class="cta-form-wrapper container">
-        <form action="handler.php" method="POST" class="cta-form">
+        <form action="\handler.php" method="POST" class="cta-form">
           <h2  class="cta-form-title">Хотите сотрудничать?</h2>
           <p class="cta-form-text">
             Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы и поможем даже в самых сложных случаях!</p>
--- handler.php
+++ handler.php
@@ -2,16 +2,16 @@
 $user_name = htmlspecialchars($_POST["username"]);
 $user_phone = htmlspecialchars($_POST["userphone"]);
 
-$token = "8014620879:AAGDkn5AyhqN3IdVHzhhhoMtgFqaRUuc04w";
-$chat_id = "-4575687290";
+$token = "8008475225:AAGszYnl3yDIF-V0WpALyNRKWX47rJ0xw1Y";
+$chat_id = "-4539025241";
 
 $formData = array(
   "Клиент: " => $user_name,
-  "Телефон: " => $user_phone
+  "Телфон: " => $user_phone
 );
 
 foreach($formData as $key => $value) {
-  $text .= $key . "<b>". urlencode($value) ."</b>" ."%0A";
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A"; 
 }
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
@@ -21,5 +21,6 @@ if ($sendToTelegram) {
 } else {
   echo "Error";
 }
+
 echo "Привет, " . $user_name . "<br>";
 echo "Ваш Телефон: <b> " . $user_phone . "</b>";
